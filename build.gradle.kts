a4234d6eedc9e1589ff790d336121b1c8925c99f
MaazX67 <maazgamer5455@gmail.com>
2026-08-29 22:25:14 +0500

    Refactor build.gradle.kts for Android configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,9 @@
 android {
-    namespace = "com.maazx.windroid"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-        targetSdk = 34
-    }
-
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17  // <-- YE 17 HONA CHAHIYE
-        targetCompatibility JavaVersion.VERSION_17  // <-- YE BHI 17
+        sourceCompatibility JavaVersion.VERSION_17
+        targetCompatibility JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "17"  // <-- YE BHI 17
+        jvmTarget = "17"
     }
-}
-
-dependencies {
-    // Compose BOM version
-    implementation(platform("androidx.compose:compose-bom:2024.09.00")) 
-}
-
-plugins {
-    id("org.jetbrains.kotlin.android") version "2.0.20" // <-- YE VERSION
 }
